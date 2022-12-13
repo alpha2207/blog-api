f2e191f0b022ca85e1e0bf8d6b47170a00135087
alpha2207 <73979701+alpha2207@users.noreply.github.com>
2022-12-13 08:38:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ const app = express();
 connectDB();
 
 const corsOptions ={
-    origin:'http://localhost:3000', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
