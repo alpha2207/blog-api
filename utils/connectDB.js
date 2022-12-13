dafd9978105e3fbcb810844a51e43d57c5a8d59f
alpha2207 <73979701+alpha2207@users.noreply.github.com>
2022-12-13 08:43:30 +0530

    Update connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 let connectDB = () => {
-    mongoose.connect('mongodb://localhost:27017/blog').then((e)=>{
+    mongoose.connect('mongodb+srv://ayanali:ayanali@blog.wnoxjvb.mongodb.net/?retryWrites=true&w=majority').then((e)=>{
         console.log("connected");
         console.log(e.connection.port);
     }).catch(e=>{
